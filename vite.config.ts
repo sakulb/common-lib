4bfc41f90bb5d8fcf9ed0781a89af72bc81a794e
lbudos <lukas.budos@petitpress.sk>
2023-01-13 14:58:42 +0100

    Add types do build.

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import path from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vuetify from 'vite-plugin-vuetify'
+import dts from 'vite-plugin-dts'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -20,5 +21,9 @@ export default defineConfig({
       },
     },
   },
-  plugins: [vue(), vuetify()],
+  plugins: [
+    vue(),
+    vuetify(),
+    dts({ insertTypesEntry: true })
+  ],
 })
